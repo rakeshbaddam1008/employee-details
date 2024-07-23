fc80d3c8529628745f0ceddd4af5e18bba998a3f
rakeshbaddam1008 <rakeshbaddam1008@gmail.com>
2024-07-23 12:32:04 -0400

    changes for env file

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
-const API_URL = "https://api.example.com/employees";
+const BASEAPI_URL = "https://api.example.com";
+const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export const submitEmployeeData = async (employeeData) => {
   try {
-    const response = await axios.post(API_URL, employeeData);
+    const response = await axios.post(`${BASEAPI_URL}/employees`, employeeData);
+    //check respone log
+    console.log(response);
     return response.data;
   } catch (error) {
+    console.error(error);
     throw error;
   }
 };
